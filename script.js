ce6dc6d3a6098dccbf634f029f45df0314f478b3
V-Emanuel <gvictor567@gmail.com>
2022-10-27 01:49:25 -0300

    Primeira tentativa de buscar os dados da API

---
+++ script.js
@@ -0,0 +1,29 @@
+let participantes = {name: ''};
+let msn
+
+
+function recebe_mensagens(){
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promessa.then(processarResposta);
+}
+
+function processarResposta(participantes) {
+    alert("Deu certo");
+	console.log(participantes.data);
+}
+recebe_mensagens();
+
+function mandar_mensagens(){
+    const mensagens = document.querySelector('caixa_mensagens');
+    for(i = 0; i < 4; i++){
+        msn = `<li class="mensagem">
+                    <p class="hora">(23:11:54)</p>
+                    <p class="nome">Jorge:</p>
+                    <p class ="msn"></p>
+                </li>`
+        mensagens.innerHTML = mensagens.innerHTML + msn;
+    }
+
+}
+
+mandar_mensagens();
