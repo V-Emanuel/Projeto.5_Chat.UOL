2c9b1aaf4ed6ff82392cff2102858e7ef8dc98e8
V-Emanuel <gvictor567@gmail.com>
2022-10-27 09:14:03 -0300

    Bônus de Tela de Entrada finalizado

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 let usuario = ''; //Nome do usuário do chat
-let msn
+let msn;
 
 /*------------------------------------------------------------------
-     Muda a página inicial quando o nome de usuário é inserido e o botão "entrar" é clicado
+     Muda a página inicial quando o nome de usuário é inserido e o botão "entrar" é clicado.
 /*------------------------------------------------------------------*/
+
 function close_initialPage(){
+    const page = document.querySelector('.entrada');
+    page.classList.remove('aparece');
+    page.classList.add('some');
+
+}
+function open_loading(){
     usuario = document.querySelector('.nome_usuario').value;
-    const initialPage = document.querySelector('.entrada');
+    const box_usuario = document.querySelector('.caixa_usuario');
+    const box_button = document.querySelector('.botao');
+    const loading = document.querySelector('.carregando');
     if(usuario != ''){
-        initialPage.classList.remove("aparece");
-        initialPage.classList.add("some");
+        box_usuario.classList.add("some");
+        box_button.classList.add("some");
+        loading.classList.remove("some");
     }
     console.log(usuario);
+    setTimeout(close_initialPage, 1500); //execulta a função 'close_initialPage()' após 1,5seg.
 }
 
 function mandar_mensagens(){
