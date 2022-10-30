ad9671ce6b7a9f96c353be94583eacd7c6a7139f
V-Emanuel <gvictor567@gmail.com>
2022-10-30 15:30:31 -0300

    Barra lateral adicionada, porém sem funcionalidade

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-let usuario = ''; //Nome do usuário do chat
+let usuario = 'Jorge', para = 'Todos', texto = 'A cascavel do piaui', tipo = 'message'; //Nome do usuário do chat
 let mensagens = [];
 
+/*------------------------------------------------------------------
+     Muda a página inicial quando o nome de usuário é inserido e o botão "entrar" é clicado.
+/*------------------------------------------------------------------*/
+function close_initialPage(){   //Função que fecha a tela inicial
+    const page_1 = document.querySelector('.pag_1');
+    const page_2 = document.querySelector('.pag_2');
+    page_1.classList.remove('aparece');
+    page_1.classList.add('some');
+    page_2.classList.remove('some');
+    page_2.classList.add('aparece');
+}
+function open_loading(){    //Função que habilita a tela de carregamento após o usuário inserir o nome
+    usuario = document.querySelector('.nome_usuario').value;
+    const box_usuario = document.querySelector('.caixa_usuario');
+    const box_button = document.querySelector('.botao');
+    const loading = document.querySelector('.carregando');
+    if(usuario != ''){
+        box_usuario.classList.add("some");
+        box_button.classList.add("some");
+        loading.classList.remove("some");
+        setTimeout(close_initialPage, 1500); //execulta a função 'close_initialPage()' após 1,5seg.
+    }  
+}
 /*------------------------------------------------------------------
     Carrega as mensagens do servidor para o código HTML
 /*------------------------------------------------------------------*/
@@ -10,9 +33,7 @@ promessa.then(respostaChegou);
 function respostaChegou(resposta){
     mensagens = resposta.data;
     rederizarMensagens();
-    console.log(mensagens)
 }
-
 function rederizarMensagens(){
     const listaMensagens = document.querySelector('.caixa_mensagens');
     for(let i = 0; i < mensagens.length; i++){
@@ -35,32 +56,22 @@ function rederizarMensagens(){
                 ${mensagem.text}</p>
             </li>`;
             document.getElementById(`msg_${i}`).style.backgroundColor = "#DCDCDC";
-        }
-        
+        }     
     } 
 }
 /*------------------------------------------------------------------
-     Muda a página inicial quando o nome de usuário é inserido e o botão "entrar" é clicado.
+    Aparece e oculta barra lateral
 /*------------------------------------------------------------------*/
-function close_initialPage(){   //Função que fecha a tela inicial
-    const page_1 = document.querySelector('.pag_1');
-    const page_2 = document.querySelector('.pag_2');
-    page_1.classList.remove('aparece');
-    page_1.classList.add('some');
-    page_2.classList.remove('some');
-    page_2.classList.add('aparece');
-
+function aparece_lateral(){
+    const barra_lateral = document.querySelector('.pag_3');
+    barra_lateral.classList.remove('some');
+    barra_lateral.classList.add('aparece');
 }
-function open_loading(){    //Função que habilita a tela de carregamento após o usuário inserir o nome
-    usuario = document.querySelector('.nome_usuario').value;
-    const box_usuario = document.querySelector('.caixa_usuario');
-    const box_button = document.querySelector('.botao');
-    const loading = document.querySelector('.carregando');
-    if(usuario != ''){
-        box_usuario.classList.add("some");
-        box_button.classList.add("some");
-        loading.classList.remove("some");
-        setTimeout(close_initialPage, 1500); //execulta a função 'close_initialPage()' após 1,5seg.
-    }
-    
+function some_lateral(){
+    const lateral = document.querySelector('.pag_3');
+    lateral.classList.add('some');
+    lateral.classList.remove('aparece');
 }
+/*------------------------------------------------------------------
+ Sinaliza que usuário entrou na sala e saiu da sala
+/*------------------------------------------------------------------*/
